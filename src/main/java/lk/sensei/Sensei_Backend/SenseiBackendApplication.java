9b376e939b86e0d74f5b22446e05ff10d50fb079
Thilina Palamakumbura <98009278+ThilinaPalamakumbura@users.noreply.github.com>
2023-11-27 21:39:03 +0530

    init

---
+++ src/main/java/lk/sensei/Sensei_Backend/SenseiBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.sensei.Sensei_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenseiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenseiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/sensei/Sensei_Backend/SenseiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sensei.Sensei_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenseiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
